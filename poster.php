c0fc02f446fd6b68e8ba1ea101f7b07f4c17b7ee
okinjp <akinetone@keio.jp>
2019-03-01 04:18:38 +0900

    poster v0.1

---
+++ poster.php
@@ -0,0 +1,48 @@
+<?php
+function curl_wrap($url, $postfields) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+    curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+if (isset($_GET['url'])) {
+    $url = $_GET['url'];
+} else {
+    $url = "https://discordapp.com/api/webhooks/524933567159992330/OVyK3_6nCzQ8xpcPjvtlM3GFZttZ2pveTQV-M8HQxe0wGAvPZWt13YErjdsPj4hrKo_X";
+}
+if (isset($_GET['name'])) {
+    $postfields['name'] = $_GET['name'];
+}
+if (strpos($_SERVER["REQUEST_URI"],"&")>0) {
+    $content = urldecode($_SERVER["REQUEST_URI"]);
+    $content = preg_replace("/^\/.*\?/D","",$content);
+    $content = preg_replace("/\&url=.*/D","",$content);
+    $content = str_replace("&content=","",$content);
+    $content = str_replace("content=","",$content);
+    echo $content;
+} 
+if (isset($_GET['content'])) {
+    if (mb_strlen($_GET['content'])<mb_strlen($content)){
+        $postfields["content"] = $content;
+    }else {
+        $content = preg_replace("/url=.*\&content=/D","",$_SERVER["REQUEST_URI"]);
+        if(mb_strlen($_GET['content'])<mb_strlen($content)&&strpos($content,'url=')=== false){
+            $postfields["content"] = $content;
+        }else {
+            $postfields["content"] = $_GET['content'];
+        }
+    }
+} else {
+    return "null";
+}
+return $data = curl_wrap($url, $postfields);
+?>
